5ea558c0d382b77a8cee8af41114434fdef78aaa
bmonagan <bmonagan@gmail.com>
2025-05-20 19:22:49 -0400

    working backwards to have the checkout button work properly

--- BEcom/accounts/views.py
+++ BEcom/accounts/views.py
@@ -2,9 +2,27 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")  # Redirect to a success page.
+    else:
+        form = AuthenticationForm()
+    return render(request, "registration/login.html", {"form": form})   
--- BEcom/payments/urls.py
+++ BEcom/payments/urls.py
@@ -1,5 +1,3 @@
-
-
 from django.urls import path
 from . import views
 app_name = 'payments'
@@ -7,7 +5,7 @@ app_name = 'payments'
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('config/', views.stripe_config),
-    path('create-checkout-session/', views.create_checkout_session),
+    path('create-checkout-session/', views.create_checkout_session, name='create-checkout-session'),
     path('success/', views.SuccessView, name='success'),
     path('cancelled/', views.CancelledView, name='cancelled'),
     path('webhook/', views.stripe_webhook)
