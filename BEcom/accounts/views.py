157c0114d9196a4c012f4b95ddf249f20519055d
bmonagan <bmonagan@gmail.com>
2025-05-23 02:23:24 -0400

    Fixed the registration page to use the new signup.html template and updated the views and urls accordingly. Will correctly get the user to register with an email.

--- BEcom/accounts/views.py
+++ BEcom/accounts/views.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -7,10 +6,11 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordResetForm
+from .forms import CustomUserCreationForm
 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm  # <-- use your custom form here
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
@@ -37,3 +37,14 @@ def password_reset(request):
     else:
         form = PasswordResetForm()
     return render(request, "registration/password_reset_form.html", {"form": form})
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home") 
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
