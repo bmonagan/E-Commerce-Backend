19ca738242c70920c6679716dcdd4103e33addeb
bmonagan <bmonagan@gmail.com>
2025-05-23 02:02:50 -0400

    Added the password reset view if i wanted to modify that more in the future. Before it was redirecting to an admin page which was not what I wanted.

--- BEcom/accounts/views.py
+++ BEcom/accounts/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordResetForm
 
 
 class SignUpView(CreateView):
@@ -22,7 +23,17 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect("home")  # Redirect to a success page.
+                return redirect("home") 
     else:
         form = AuthenticationForm()
-    return render(request, "registration/login.html", {"form": form})   
+    return render(request, "registration/login.html", {"form": form})  
+
+def password_reset(request):
+    if request.method == "POST":
+        form = PasswordResetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("password_reset_done")
+    else:
+        form = PasswordResetForm()
+    return render(request, "registration/password_reset_form.html", {"form": form})
--- BEcom/payments/urls.py
+++ BEcom/payments/urls.py
@@ -5,7 +5,7 @@ app_name = 'payments'
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('config/', views.stripe_config),
-    path('create-checkout-session/', views.create_checkout_session, name='create-checkout-session'),
+    path('create-checkout-session/', views.create_checkout_session, name='create_checkout_session'),
     path('success/', views.SuccessView, name='success'),
     path('cancelled/', views.CancelledView, name='cancelled'),
     path('webhook/', views.stripe_webhook)
